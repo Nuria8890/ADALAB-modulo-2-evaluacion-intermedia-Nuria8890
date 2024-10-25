62d9319d3a45ac17167cf2bc48a87e785a2ce42b
Nuria Campo <nuria.campo7@gmail.com>
2024-10-25 10:25:41 +0200

    refactoring function compare numbers

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const input = document.querySelector('.js-input')
+const input = document.querySelector('.js-input');
 const button = document.querySelector('.js-button');
 const track = document.querySelector('.js-track');
 const attempts = document.querySelector('.js-attempts');
@@ -17,9 +17,17 @@ const randomNumber = getRandomNumber(max);
 // Cuando hago click en el botón "Prueba" accedo al contenido del input
 button.addEventListener('click', (event) => {
   event.preventDefault();
+  compareNumbers();
+  
+
+  // Actualizar el contador de intentos cada vez que la jugadora pruebe
+    numberAttempts += 1;
+    attempts.innerHTML = `Número de intentos: ${numberAttempts}`;
+});
+
+const compareNumbers = () => {
   const userNumber = parseFloat(input.value);
 
-  // Comparar el userNumber con el número aleatorio
     if(userNumber === randomNumber) {
       track.innerHTML = 'Has ganado campeona!!!'
     } else if (userNumber < randomNumber && userNumber > 1) {
@@ -29,9 +37,6 @@ button.addEventListener('click', (event) => {
     } else if (userNumber < 1 || userNumber > 100) {
       track.innerHTML = 'Pista: El número debe estar entre 1 y 100'
     }
+}
 
-  // Actualizar el contador de intentos cada vez que la jugadora pruebe
-    numberAttempts += 1;
-    attempts.innerHTML = `Número de intentos: ${numberAttempts}`;
-});
 
