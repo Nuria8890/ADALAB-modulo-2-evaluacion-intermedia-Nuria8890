e7fcf36efcff104470f00d30ae84fb6498f31291
Nuria Campo <nuria.campo7@gmail.com>
2024-10-25 09:28:52 +0200

    gender random number and click 'Prueba'

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+const input = document.querySelector('.js-input')
+const button = document.querySelector('.js-button');
+
+// Generar número aleatorio
+const max = 100;
+
+function getRandomNumber(max) { 
+  return Math.ceil(Math.random() * max); 
+}
+console.log('El número aleatorio es:', getRandomNumber(max));
+
+
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const userNumber = parseFloat(input.value);
+  console.log('El número introducido por la usuaria es:', userNumber);
+});
