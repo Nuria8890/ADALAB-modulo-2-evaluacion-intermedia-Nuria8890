943fc0e8335862c0714a8cf9a7dca030e4bf818c
Nuria Campo <nuria.campo7@gmail.com>
2024-10-25 09:37:48 +0200

    store random number in a constant

--- js/main.js
+++ js/main.js
@@ -10,12 +10,22 @@ const max = 100;
 function getRandomNumber(max) { 
   return Math.ceil(Math.random() * max); 
 }
-console.log('El número aleatorio es:', getRandomNumber(max));
+const randomNumber = getRandomNumber(max);
+console.log('El número aleatorio es:', randomNumber);
 
-
-
+// Cuando hago click en el botón "Prueba" accedo al contenido del input
 button.addEventListener('click', (event) => {
   event.preventDefault();
   const userNumber = parseFloat(input.value);
   console.log('El número introducido por la usuaria es:', userNumber);
+
+  if(userNumber === randomNumber) {
+    console.log('el número es el mismo')
+  } else {
+    console.log('El número es diferente')
+  }
+  
 });
+
+// Comparar el userNumber con el número aleatorio
+
